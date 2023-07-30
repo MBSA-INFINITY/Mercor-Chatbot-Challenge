b44a3a63d75616711b837cb69055e796e17d1d5f
M.B.SAI ADITYA <mbsaiaditya@gmail.com>
2023-07-30 16:37:08 +0530

    removed api key

--- db.py
+++ db.py
@@ -2,4 +2,6 @@ import pymongo
 connection_string = "mongodb://localhost:27017"
 db_client = pymongo.MongoClient(connection_string)
 DB_NAME="budgetgpt"
-db_client = db_client.get_database("users")
+db_client = db_client.get_database(DB_NAME)
+
+users_collection = db_client["user_details"]
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from langchain.llms import OpenAI
 from langchain import PromptTemplate, HuggingFaceHub, LLMChain
 import os
 from typing import List
+from db import users_collection
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 # Load your OpenAI API key
 models.OpenAI.api_key = OPENAI_API_KEY
-# models.OpenAI.api_key = "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"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -32,10 +32,13 @@ question_template = """{question}"""
 question_prompt = PromptTemplate(template=question_template, input_variables=["question"])
 question_llm_chain = LLMChain(prompt=question_prompt, llm=llm)
 
-total_user_balance = 0
 @textbase.chatbot("talking-bot")
 def on_message(message):
-    global total_user_balance
+    print(message.content, type(message))
+    if user_details := users_collection.find_one({"user_id": "local"},{"_id": 0}):
+        total_user_balance = user_details.get("amount")
+    else:
+        total_user_balance = 0
     print(total_user_balance)
     result = llm_chain.run(message) 
     result = result.lower().split("\n")
@@ -44,16 +47,19 @@ def on_message(message):
     print(result)
     if len(result) == 4:
         source = result[0]
-        amount = int(result[2])
         credit = True if result[1] == "credit" else False
         if source == "statement":
+            amount = int(result[2])
             result = f"Your previous balance was {total_user_balance}. "
             if credit:
+                users_collection.update_one({"user_id": "local"},{"$inc":{"amount": amount}})
                 total_user_balance+=amount
             else:
+                users_collection.update_one({"user_id": "local"},{"$inc":{"amount": -amount}})
                 total_user_balance-=amount
             result += f"Your Total Balnce now is {total_user_balance}."
         else:
-            new_query = f"My current balance is {total_user_balance}. {message}"
-            result = llm_chain.run(new_query) 
+            new_query = f"{message.content}. My current balance is {total_user_balance}. Give answer in one line!"
+            print("new_query: ", new_query)
+            return question_llm_chain.run(new_query) 
     return result
